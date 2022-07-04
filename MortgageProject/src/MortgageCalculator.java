956bf2431e9dcc6019929ec2837623d8220e49c9
Sai Kaung Khant <kaungkaung.m@gmail.com>
2022-07-04 14:16:17 -0500

    update

--- MortgageProject/src/MortgageCalculator.java
+++ MortgageProject/src/MortgageCalculator.java
@@ -1,62 +1,64 @@
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MortgageCalculator {
+	List<String> Level1Profession = new ArrayList<>(Arrays.asList(new String[]{"Developer","Architect","Scrum master"}));
+	List<String> Level2Profession = new ArrayList<>(Arrays.asList(new String[]{"Tester","System Adminstrator","Technical writer"}));
+	List<String> Level3Profession = new ArrayList<>(Arrays.asList(new String[]{"Department head","Professor"}));
 	
 	public double computeMaxMortgage(int yearOfBirt, int month, int day, double monthlyIncome, boolean married, double monthlyIncomePartner, String profession) {
 		double result=0;
 		//calculate age
 		LocalDate today = LocalDate.now();                          //Today's date
 		LocalDate birthday = LocalDate.of(yearOfBirt, month, day);  //Birth date
+		
+		
 		 
 		Period p = Period.between(birthday, today);
 		int age = p.getYears();
 		System.out.println(age);
-
-		if (! married) {
-			if (age >18 && 2000<=monthlyIncome && monthlyIncome<3000) {
-				if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 160000;
-				if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 120000;
-				if (profession.equals("Department head") || profession.equals("Professor") ) result = 220000;
-			}
-			if (age >18 && 3000<=monthlyIncome && monthlyIncome<5000) {
-				if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 180000;
-				if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 140000;
-				if (profession.equals("Department head") || profession.equals("Professor") ) result = 250000;
-			}
-			if (age >18 && 5000<=monthlyIncome) {
-				if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 220000;
-				if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 160000;
-				if (profession.equals("Department head") || profession.equals("Professor") ) result = 280000;
-			}
-			if (age < 18) {
-				result = 0;
-			}
+		
+		if (age < 18) {
+			result = 0;
 		}
-		else {
-			double totalIncome = monthlyIncome + monthlyIncomePartner * 0.94; 
-			if (age >18 && 2000<=totalIncome && totalIncome<3000) {
-				if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 160000;
-				if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 120000;
-				if (profession.equals("Department head") || profession.equals("Professor") ) result = 220000;
-			}
-			if (age >18 && 3000<=totalIncome && totalIncome<5000) {
-				if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 180000;
-				if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 140000;
-				if (profession.equals("Department head") || profession.equals("Professor") ) result = 250000;
-			}
-			if (age >18 && 5000<=totalIncome) {
-				if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 220000;
-				if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 160000;
-				if (profession.equals("Department head") || profession.equals("Professor") ) result = 280000;
-			}
-			if (age < 18) {
-				result = 0;
-			}
 
+		
+		double totalIncome = married ? monthlyIncome + monthlyIncomePartner * 0.94 : monthlyIncome; 
+		
+		if (2000<=totalIncome && totalIncome<3000 && Level1Profession.contains(profession)) {
+			result = 160000;
 		}
+		if (2000<=totalIncome && totalIncome<3000 && Level2Profession.contains(profession)) {
+			result = 120000;
+		}
+		if (2000<=totalIncome && totalIncome<3000 && Level3Profession.contains(profession)) {
+			result = 220000;
+		}
+		
+		if (2000<=totalIncome && totalIncome<3000) {
+			if (Level1Profession.contains(profession)) result = 160000;
+			if (Level2Profession.contains(profession)) result = 120000;
+			if (Level3Profession.contains(profession)) result = 220000;
+		}
+		if (3000<=totalIncome && totalIncome<5000) {
+			if (Level1Profession.contains(profession)) result = 180000;
+			if (Level2Profession.contains(profession)) result = 140000;
+			if (Level3Profession.contains(profession)) result = 250000;
+		}
+		if (5000<=totalIncome) {
+			if (Level1Profession.contains(profession)) result = 220000;
+			if (Level2Profession.contains(profession)) result = 160000;
+			if (Level3Profession.contains(profession)) result = 280000;
+		}
+		
+
+		
 		
 		return result;
 	}
+	
 
 }
--- ShoppingCart/src/shoppingcart/ShoppingCart.java
+++ ShoppingCart/src/shoppingcart/ShoppingCart.java
@@ -12,14 +12,14 @@ public class ShoppingCart {
 	public void action(Product product, String action) {
 		if (action.equals("add")){
 		for (CartLine cline : list) {
-			if (cline.getP().getProductnumber().equals(product.getProductnumber())) {
-				cline.setQ(cline.getQ()+1);
+			if (cline.getProduct().getProductnumber().equals(product.getProductnumber())) {
+				cline.setQuantity(cline.getQuantity()+1);
 				return;
 			}
 		}
 		CartLine cline = new CartLine();
-		cline.setP(product);
-		cline.setQ(1);
+		cline.setProduct(product);
+		cline.setQuantity(1);
 		list.add(cline);
 		}
 		else{
@@ -27,9 +27,9 @@ public class ShoppingCart {
 			Iterator<CartLine> iter = list.iterator();
 			while (iter.hasNext()){
 				CartLine cline = iter.next();
-				if (cline.getP().getProductnumber().equals(product.getProductnumber())){
-					if (cline.getQ()>1){
-						cline.setQ(cline.getQ()-1);
+				if (cline.getProduct().getProductnumber().equals(product.getProductnumber())){
+					if (cline.getQuantity()>1){
+						cline.setQuantity(cline.getQuantity()-1);
 					}
 					else{
 						iter.remove();
@@ -40,10 +40,10 @@ public class ShoppingCart {
 			else{ //action is print
 				System.out.println("Content of the shoppingcart:");
 				for (CartLine cline : list) {
-					System.out.println(cline.getQ() + " "
-							+ cline.getP().getProductnumber() + " "
-							+ cline.getP().getDescription() + " "
-							+ cline.getP().getPrice());
+					System.out.println(cline.getQuantity() + " "
+							+ cline.getProduct().getProductnumber() + " "
+							+ cline.getProduct().getDescription() + " "
+							+ cline.getProduct().getPrice());
 				}
 				System.out.println("Total price ="+getT());
 			}
@@ -55,7 +55,7 @@ public class ShoppingCart {
 	public double getT(){
 		double tp = 0.0;
 		for (CartLine c : list) {
-			tp=tp+(c.getP().getPrice() * c.getQ());
+			tp=tp+(c.getProduct().getPrice() * c.getQuantity());
 		}
 		return tp;
 	}
